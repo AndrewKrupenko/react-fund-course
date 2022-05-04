3fc31280467e426f24ae178274f63dc1fe740a85
AndrewKrupenko <andrykrupenko338@gmail.com>
2022-05-05 00:43:22 +0300

    added posts hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './styles/App.css'
-import {useMemo, useState} from "react"
+import {useState} from "react"
 import PostList from "./components/PostList"
 import PostForm from "./components/PostForm"
 import PostFilter from "./components/PostFilter"
 import MainModal from "./components/UI/modal/MainModal"
 import MainButton from "./components/UI/button/MainButton"
+import {usePosts} from "./hooks/usePosts"
 
 function App() {
 
@@ -28,20 +29,7 @@ function App() {
 
   const [filter, setFilter] = useState({sortBy: '', searchQuery: ''})
   const [isModalVisible, setIsModalVisible] = useState(false)
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sortBy) {
-      return [...posts].sort((a, b) => a[filter.sortBy].localeCompare(b[filter.sortBy]))
-    }
-    return posts
-  }, [filter.sortBy, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post =>
-      post.title.toLowerCase().includes(filter.searchQuery) ||
-      post.description.toLowerCase().includes(filter.searchQuery)
-    )
-  }, [filter.searchQuery, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sortBy, filter.searchQuery)
 
   const createPost = newPost => {
     setPosts([...posts, newPost])
---
+++ src/hooks/usePosts.js
@@ -0,0 +1,25 @@
+import {useMemo} from "react"
+
+export const useSortedPosts = (posts, sortBy) => {
+  const sortedPosts = useMemo(() => {
+    if (sortBy) {
+      return [...posts].sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+    }
+    return posts
+  }, [sortBy, posts])
+
+  return sortedPosts
+}
+
+export const usePosts = (posts, sortBy, searchQuery) => {
+  const sortedPosts = useSortedPosts(posts, sortBy)
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post =>
+      post.title.toLowerCase().includes(searchQuery) ||
+      post.description.toLowerCase().includes(searchQuery)
+    )
+  }, [searchQuery, sortedPosts])
+
+  return sortedAndSearchedPosts
+}
